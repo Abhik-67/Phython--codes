77a4e307348227a5113d4a7d178540dcd540c8d7
ABHIK LODH <88699987+Abhik-67@users.noreply.github.com>
2021-11-19 10:44:19 +0530

    Update Maximum_Partition.py

--- Maximum_Partition.py
+++ Maximum_Partition.py
@@ -19,6 +19,17 @@ Approach:
 You can have K = 3 partitions. So, P₁=1, P2=-2, P3=3
 The sum obtained is P1 P2 + P3-1-(-2) +3=6.
 
+Input :
+The first line of input consist of an integer N, which denotes the number of integers in the array.    
+The second line of input consist of N integers, which denotes the values of the array.
+
+Output :
+A single integer which denotes the maximum value of the Special Sum that can be obtained by any number of partitions.
+
+Explanation
+You can make three partitions , first from [0,0], second from [1,2] and third from [3,4].
+Special value of such partions = 1 - (-2 + -3) + (2 + 1) = 9.
+
 Input:
 3
 [1,-2,3]
@@ -26,6 +37,10 @@ Output:
 6
 
 Input:
+5
+[1,-2,-3,2,1]
+Output:
+9
 
 
 
